5d196c96d7ba8f6f3672c5b87e7a97753ec76ed5
Tori Fortune <toriolley@gmail.com>
2019-06-18 23:48:33 -0500

    updated seeds file to include more meals and recipes

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,82 @@ INSERT INTO recipe (name_of_meal, descriptions, ingredients, prep_time, caloric_
 Preheat an outdoor grill for medium heat, and lightly oil the grate. Remove the salmon from the marinade, and shake off excess. Discard the remaining marinade.
 Grill the salmon on the preheated grill until browned and the fish flakes easily with a fork, about 5 minutes on each side.", 1, 3);
    
+INSERT INTO users (user_name, email, password) VALUES ("Fortune", "Fortune@hotmail.com", "123qwe");
+INSERT INTO recipe_type (meal_type) VALUES ("Breakfast"), ("Lunch"), ("Dinner");
+INSERT INTO recipe (name_of_meal, descriptions, ingredients, prep_time, caloric_content, servings, cooking_instructions, user_id, recipe_type_id) VALUES ("Nigerian Jollof rice", "A spicy and vibrant one-pot tomato rice dish  served as a staple in Nigeria","3¾ cups (750g) long grain parboiled rice
+2-3 cups Tomato Stew
+Chicken (whole chicken, drumsticks or chicken breast)
+Pepper and salt (to taste)
+2 medium onions
+3 Knorr cubes
+2 teaspoons thyme
+2 teaspoons curry powder (Nigerian curry powder)", "45 minutes", "250 calories", "4 servings", "PREPARATION
+In a blender, combine tomatoes, scotch bonnet pepper and onions; purée. Pour out half the purée into a bowl; set aside. Add the bell peppers to the purée remaining in the blender and pulse until smooth. Add to the mixture that was set aside and stir to combine.
+Heat vegetable oil in a large pot over medium heat. Add blended vegetables along with the salt, curry powder, ground chile pepper, garlic powder, onion powder, bay leaves, ginger and thyme. Bring mixture to a boil.
+Stir in the rice until well mixed, then reduce the heat to low.
+Cover pot and let cook until rice is al dente, about 45 minutes. Check after 30 minutes; if rice is sauce-logged, remove the lid to cook off the excess sauce. If rice seems dry, stir in 1 to 2 cups water. Allow the rice at the bottom of the pot to char a bit to infuse it with a smoky flavor.", 2, 3);
+   
+
+
+
+INSERT INTO users (user_name, email, password) VALUES ("Fortune", "Fortune@hotmail.com", "123qwe");
+INSERT INTO recipe_type (meal_type) VALUES ("Breakfast"), ("Lunch"), ("Dinner");
+INSERT INTO recipe (name_of_meal, descriptions, ingredients, prep_time, caloric_content, servings, cooking_instructions, user_id, recipe_type_id) VALUES ("Efo Riro", "A spicy and vibrant one-pot tomato rice dish  served as a staple in Nigeria","Beef
+Shaki (cow tripe)
+Smoked fish
+Dry fish
+Stock fish
+20 cl palm oil
+500g Efo Shoko
+5 tatashe peppers
+2 tablespoons ground crayfish
+2 red onions
+2 small stock cubes
+2 tablespoons locust beans (iru)
+Salt & Habanero / Scotch Bonnet peppers (to taste)", "45 minutes", "180 calories", "4 servings", "Before you cook Efo Riro
+If you are using the hard stock fish, soak it for a few hours. Soak the dry fish till soft and debone.
+Cut the leafy vegetables
+Deseed the tatashe and grind till coarse.
+Prepare other ingredients: pound/blitz the pepper, dice the onions and grind the crayfish.
+Cooking Directions
+Start cooking the shaki first with as little water as possible (see video below) as it is the toughest meat in the bunch. This soup should have as little water as possible so add small amounts of water at a time and top it up as you cook.
+When the shaki starts to curl, add the dry fish and stockfish.
+When the shaki is almost done, add beef, stock cubes and some of the onions and cook till all the meat and fish are well done. Set these aside.
+In another pot, pour the palm oil and heat it up. Once hot, add the remaining onions.
+Fry the tatashe peppers till there's no more water in it. This should take about 15 minutes.
+Add the locust beans, crayfish, and stir very well.
+Add the cooked meat and fish, stir very well.
+Add the vgetables, stir very well, cover and once it heats up again, add salt to taste and take the pot off the stove.
+Serve Efo Riro with Semolina, Amala, Pounded Yam, Eba or any other fufu meal. It also goes well with boiled white rice.", 2, 3);
+   
+
+
+
+INSERT INTO users (user_name, email, password) VALUES ("Fortune", "Fortune@hotmail.com", "123qwe");
+INSERT INTO recipe_type (meal_type) VALUES ("Breakfast"), ("Lunch"), ("Dinner");
+INSERT INTO recipe (name_of_meal, descriptions, ingredients, prep_time, caloric_content, servings, cooking_instructions, user_id, recipe_type_id) VALUES ("Seafood Okra Soup", "A hearty soup dish often served with a side of eba, rice or fufu (see instruction for named sides)","30 okra fingers
+2 medium fish
+2 cups shrimps
+500g beef
+¼ cup red palm oil
+2 cups of chopped spinach
+1 sweet pepper
+1 red onion
+Habanero peppers (to your taste)
+4 small seasoning cubes
+Salt (to your taste)", "30 minutes", "200 calories", "4 servings", "PREPARATION. Before you prepare Seafood Okra Soup
+Clean the fish and then cut them into sizeable pieces.
+Clean and de-vein the shrimps.
+Hand cut or grate the okra, you can also use a blender if you prefer a smoother consistency.
+Chop or blend your peppers.
+Dice the onions.
+Directions for making the Seafood Okra Soup
+Cook the beef with the stock cubes and half of the diced onions.
+When done, remove the beef and add the fish. Cook for 5 minutes, at most 7 minutes so it does not disintegrate then gently take them out.
+Add the shrimps and cook for 5 minutes and take them out.
+In a different pot, add the red palm oil, once it heats up, add the remaining onions, fry for a bit.
+Add the okra and fry till it heats up all over.
+Start adding the stock bit by bit and stir very well to activate the elasticity of the okra. When all the stock have been added, add the peppers and the spinach. Cover and cook for 1 minute maximum.
+Add the fish and shrimps, add salt of necessary and stir very well.
+The Seafood Okra Soup is ready to be served with any Nigerian Fufu Meal including low carb fufu meals.", 2, 3);
+    
