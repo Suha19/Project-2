c4f6f2ba35da07485d2efa34f2522decbcfbeba3
Juan Sebastian Delgado Rincon <juansebastiandelgadorincon@Juans-MacBook-Pro.local>
2019-06-19 12:44:48 -0500

    Recipes added on the seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -82,4 +82,172 @@ Add the okra and fry till it heats up all over.
 Start adding the stock bit by bit and stir very well to activate the elasticity of the okra. When all the stock have been added, add the peppers and the spinach. Cover and cook for 1 minute maximum.
 Add the fish and shrimps, add salt of necessary and stir very well.
 The Seafood Okra Soup is ready to be served with any Nigerian Fufu Meal including low carb fufu meals.", 2, 3);
-    
+
+
+
+
+INSERT INTO users (user_name, email, password) VALUES ("Juan","juansebastiandelgado@hotmail.com","12345");
+INSERT INTO recipe_type (meal_type) VALUES ("Breakfast"), ("Lunch"), ("Dinner");
+INSERT INTO recipe (name_of_meal, descriptions, ingredients, prep_time, caloric_content, servings, cooking_instructions, user_id, recipe_type_id) VALUES (
+"Paisa Platter",
+"This is a typical meal popular in Colombian cuisine; The main characteristic of this dish is the generous amount and variety of food",
+"1 pound skinless pork belly
+2 teaspoons kosher salt
+2 teaspoons granulated sugar
+1/2 teaspoon ground cumin
+1/4 cup canola oil
+2 medium-size yellow onions, chopped (about 4 cups)
+1 large green bell pepper, chopped (about 1 1/2 cups)
+1 tablespoon achiote paste
+4 plum tomatoes, chopped (about 5 cups)
+5 garlic cloves, chopped
+2 teaspoons ground cumin
+1 3/4 teaspoons kosher salt
+1/4 cup chopped fresh cilantro
+3/4 pound dried large red beans (about 1 3/4 cups)
+1 tablespoon canola oil
+1/4 pound skinless pork belly, cut into 1-in. pieces
+8 cups lower-sodium chicken broth
+2 cups Hogao
+1 teaspoon ground cumin
+3/4 teaspoon kosher salt
+1 tablespoon canola oil
+1 pound ground chuck
+1 cup Hogao
+1 teaspoon kosher salt
+1/2 teaspoon ground cumin
+1 tablespoon canola oil
+4 (2 1/2-oz.) fresh Mexican chorizo links
+1 cup (about 5 oz.) white masarepa (pre-cooked corn flour)
+4 ounces asadero cheese or queso blanco, shredded (about 1 cup)
+1 cup warm water
+3/4 teaspoon kosher salt
+3 tablespoons canola oil, divided
+2 large ripe plantains, peeled
+1/4 cup canola oil
+1/4 teaspoon kosher salt
+2 tablespoons canola oil
+4 large eggs
+1/4 teaspoon kosher salt
+1/8 teaspoon black pepper
+3 cups cooked white rice
+2 avocados, halved
+Fresh cilantro leaves",
+"120 minutes",
+"1000 calories",
+"4 servings",
+"PREPARATION:
+Step 1
+Prepare the Chicharrones: Place pork belly on a rimmed baking sheet. Stir together salt, sugar and cumin; rub evenly on pork. Wrap pork in plastic wrap, and chill 6 to 12 hours.
+Step 2
+Preheat oven to 425°F. Unwrap pork, and place, fat side up, on a lightly greased wire rack. Place rack in a rimmed baking sheet, and bake until golden brown and crispy, about 30 minutes.
+Step 3
+Reduce oven temperature to 250°F, and cook until pork is tender when pierced with a fork, about 1 hour and 15 minutes.
+Step 4
+Cool to room temperature, about 45 minutes. Wrap pork in plastic wrap, and chill until firm, at least 1 hour.
+Step 5
+Cut pork into 4 strips. Cut 3/4-inch deep slits crosswise in pork at 1-inch intervals. Heat a large skillet over medium. Cook pork until golden brown and crispy, about 4 minutes per side.
+Step 6
+Prepare the Hogao: Heat oil in a large skillet over medium. Add onions, bell pepper, and achiote paste; cook, stirring occasionally, until tender, about 8 minutes. Stir in tomatoes, garlic, cumin, and salt. Cook, stirring often, until tomatoes have broken down and thickened, about 15 minutes. Remove from heat, and stir in cilantro.
+Step 7
+Prepare the Frijoles Rojos: Place beans in a medium bowl; add water to cover by at least 2 inches. Cover and let stand overnight at room temperature.
+Step 8
+Drain beans. Heat oil in a Dutch oven over medium-low. Add pork belly; cook, stirring occasionally, until browned, about 8 minutes. Stir in beans, broth, Hogao, cumin, and salt. Increase heat to medium-high, and bring to a boil. Reduce heat to medium-low, and simmer, stirring occasionally, until beans are tender, 4 hours and 30 minutes to 5 hours, adding water, 1 cup at a time, if mixture becomes too thick.
+Step 9
+Prepare the Carne Molida: Heat oil in a large skillet over medium-high. Add beef; cook, stirring to break into pieces, until browned, 6 to 8 minutes. Stir in Hogao, salt, and cumin; cook, stirring often, until well combined and heated through, about 2 minutes.
+Step 10
+Prepare the Chorizo: Heat oil in a large cast-iron skillet over medium. Cook chorizo links, turning occasionally, until lightly charred and cooked through, about 15 minutes.
+Step 11
+Prepare the Arepas: Combine masarepa, cheese, water, salt, and 1 tablespoon of the oil in a medium bowl. Stir with a wooden spoon until dough is smooth. (Dough will look soupy, but will firm up after standing.) Cover and let stand 10 minutes.
+Step 12
+Shape dough into 4 (4-ounce) balls. Gently flatten each into a 1/2-inch-thick disk (about 3 1/2 inches in diameter). Heat remaining 2 tablespoons oil in a large cast-iron skillet over medium. Cook arepas until golden brown and charred in spots, about 5 minutes per side.
+Step 13
+Prepare the Tajadas de Plátano: Cut plantains diagonally into 1/2-inch-thick slices. Heat oil in a large nonstick skillet over medium. Cook plantains, in batches, until golden brown, 1 to 2 minutes per side. Transfer to a plate lined with paper towels, and sprinkle with salt.
+Step 14
+Prepare the Huevos Fritos: Heat oil in a large nonstick skillet over medium. Break eggs into pan, and sprinkle with salt and pepper. Cook until whites are set and starting to brown around edges, 4 to 5 minutes.
+Step 15
+Evenly divide Chicharrones, Frijoles Rojos, Carne Molida, Chorizo, Arepas, Tajadas de Plátano, Huevos Fritos, rice, avocados, and remaining Hogao among 4 platters. Garnish with cilantro, and serve immediately",
+2,
+3);
+
+
+
+
+INSERT INTO users (user_name, email, password) VALUES ("Juan","juansebastiandelgado@hotmail.com","12345");
+INSERT INTO recipe_type (meal_type) VALUES ("Breakfast"), ("Lunch"), ("Dinner");
+INSERT INTO recipe (name_of_meal, descriptions, ingredients, prep_time, caloric_content, servings, cooking_instructions, user_id, recipe_type_id) VALUES (
+"Ajiaco (Potato, Corn, and Chicken Stew)",
+"Is a popular dish typically made with chicken, three varieties of potatoes, and the Galinsoga parviflora herb, commonly referred to in Colombia as guasca and in the U.S.",
+"1 tablespoon olive oil, divided
+12 chicken thighs (about 3 pounds), skinned
+1/2 cup chopped onion
+1/2 cup thinly sliced carrot
+3 cups fat-free, less-sodium chicken broth
+1 cup fresh corn kernels (about 2 ears)
+1 1/2 teaspoons chopped fresh oregano
+1 teaspoon chopped fresh thyme
+2 1/2 cups finely shredded peeled baking potato
+2 1/2 cups cubed peeled Yukon gold potato (about 1 pound)
+1/4 cup chopped fresh cilantro
+1 tablespoon fresh lime juice
+1 teaspoon salt
+1/2 teaspoon hot pepper sauce
+1/4 teaspoon freshly ground black pepper",
+"30 minutes",
+"331 calories",
+"6 servings",
+"PREPARATION: 
+Step 1
+Heat 1 1/2 teaspoons oil in a large Dutch oven over medium-high heat. Add half of chicken; cook 5 minutes, browning on all sides. Remove chicken from pan. Repeat procedure with remaining oil and chicken. Remove from pan; cover and keep warm.
+Step 2
+Add onion and carrot to pan; sauté 2 minutes. Stir in broth, corn, oregano, and thyme; bring to a simmer. Stir in baking potato and chicken; cover and simmer 20 minutes, stirring frequently to keep potato from sticking to pan. Add cubed potato; cover and simmer 20 minutes, stirring frequently. Stir in the cilantro and remaining ingredients, and cook, uncovered, 5 minutes, stirring frequently.",
+2,
+3);
+
+
+
+
+INSERT INTO users (user_name, email, password) VALUES ("Juan","juansebastiandelgado@hotmail.com","12345");
+INSERT INTO recipe_type (meal_type) VALUES ("Breakfast"), ("Lunch"), ("Dinner");
+INSERT INTO recipe (name_of_meal, descriptions, ingredients, prep_time, caloric_content, servings, cooking_instructions, user_id, recipe_type_id) VALUES (
+"Colombian Empanadas",
+"Empanadas are a popular snack in Colombia and are served by most Colombian restaurants in the USA. Traditionally, these delicious fritters are made with shredded pork and beef, but in my family we always make them with ground meat.",
+"Vegetable oil for frying
+Lime and ají for serving
+Dough or Masa:
+1 ½ cups precooked yellow cornmeal (masarepa)
+2 cups water
+1 tablespoon vegetable oil
+½ tablespoon sazon Goya with azafran
+ ½ teaspoon Salt
+Filling:
+2 cups peeled and diced white potatoes
+1 chicken or vegetable bouillon tablet
+1 tablespoon olive oil
+¼ cup chopped white onions
+1 cup chopped tomato
+½ teaspoon salt
+¼ cup chopped green onions
+1 chopped garlic clove
+2 tablespoon chopped fresh cilantro
+2 tablespoon chopped red bell pepper
+¼ teaspoon black pepper
+½ pound ground pork and beef",
+"60 minutes",
+"120 calories",
+"20 servings",
+"PREPARATION: 
+To prepare the dough:Place the masarepa in a large bowl. Add the sazon Goya and salt and stir to mix well. Add the water and oil and mix to form dough. Pat the dough into a ball and knead for 2 minutes or until smooth. Cover with plastic and set aside for 20 minutes.
+Meanwhile, to make the filling, cook the potatoes in a pot with water and the bouillon tablet for 20-25 minutes or until tender. Drain and gently mash the potatoes. Set aside.
+Heat 1 tablespoon olive oil in a large, heavy skillet. Add the onion and cook over medium-low heat stirring frequently, for 5 minutes. Add the tomatoes, green onions, garlic, bell pepper, cilantro, salt and black pepper. Cook for about 15 minutes.
+Add the ground pork and beef. Cook, breaking up the meat with a wooden spoon, for 10 to 15 minutes or until the mixture is fairly dry.
+Transfer the meat mixture to the mashed potatoes bowl and mix well to combine.
+Break small portions of the dough, about 1 ½ tablespoons each one, and form each portion into a ball by rolling between the palms of your hands.
+Place the balls of dough between two pieces of plastic and roll each out very thinly to form a circle. Remove the top plastic and place 1 tablespoon of the filling in the center of each.
+Then using the plastic underneath, fold the dough over to enclose the filling, forming a half circle. Tightly seal the edges by crimping with the tines of a fork.
+Fill a large pot with vegetable oil and heat over medium heat to 360° F.
+Carefully place 3 or 4 empanadas at the time in the heated oil and fry for about 2 minutes until golden on all sides.
+Using a slotted spoon transfer the empanadas to a plate lined with paper towels. Serve with ají and lime on the side.",
+2,
+3);
+
