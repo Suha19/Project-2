feb7096697cc622178f761bfe7bf6a37f9940448
Suha19 <47680905+Suha19@users.noreply.github.com>
2019-06-19 14:59:44 -0500

    merging user login and user register to the add recipe

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 var db = require("./models");
 var express = require('express');
 var session = require("express-session");
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var app = express();
 var passport = require('passport');
 var PORT = process.env.PORT || 1100;
@@ -12,16 +12,16 @@ var PORT = process.env.PORT || 1100;
 // Sets up the Express app to handle data parsing
 // =============================================================
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // Static directory
 // =============================================================
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static("public"));
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -30,6 +30,7 @@ app.use(passport.session());
 // =============================================================
 require("./routes/html-routes.js")(app);
 require("./routes/recipes-api-routes.js")(app);
+require("./routes/login-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
